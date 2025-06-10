6a0461d39f284b1c0d654aad04a17a90e818d68b
JebCodeOficial <jebcode@outlook.com.br>
2025-06-10 14:20:49 -0300

    Desafio modulo 31

--- mod21/ExplicacaoLambda.java
+++ mod21/ExplicacaoLambda.java
@@ -1,9 +1,7 @@
-// Expressão normal (forma tradicional)
 interface Calculadora {
     int operacao(int a, int b);
 }
 
-// Implementação tradicional
 class Soma implements Calculadora {
     @Override
     public int operacao(int a, int b) {
@@ -11,10 +9,8 @@ class Soma implements Calculadora {
     }
 }
 
-// Uso tradicional
 Calculadora calc = new Soma();
 int resultado = calc.operacao(5, 3);
 
-// Agora a mesma coisa usando expressão lambda
 Calculadora calculadoraLambda = (a, b) -> a + b;
 int resultadoLambda = calculadoraLambda.operacao(5, 3);
--- mod21/lista.java
+++ mod21/lista.java
@@ -3,16 +3,14 @@ import java.util.List;
 
 public class lista {
     public static void main(String[] args) {
-        // Forma tradicional
+
         List<String> nomes = Arrays.asList("João", "Maria", "Pedro");
         for(String nome : nomes) {
             System.out.println(nome);
         }
 
-        // Com expressão lambda
         nomes.forEach(nome -> System.out.println(nome));
         
-        // Ou ainda mais conciso
         nomes.forEach(System.out::println);
     }
 }
